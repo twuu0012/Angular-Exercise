9ef27568ee77ea4ca1498d9e3f0804374634b4a1
twuu0012 <astroneer976@gmail.com>
2020-04-30 11:52:19 +1000

    Updated server component

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Server } from '../../shared/server';
+
+
+const SAMPLE_SERVERS: any[] = [
+  { id: 1, name: 'dev', isOnline: true },
+  { id: 2, name: 'staging', isOnline: false },
+  { id: 3, name: 'prod', isOnline: true },
+  { id: 4, name: 'mail', isOnline: false },
+]
 
 @Component({
   selector: 'app-section-health',
@@ -8,6 +17,8 @@ import { Component, OnInit } from '@angular/core';
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
+
+  servers: Server[] = SAMPLE_SERVERS;
 
   ngOnInit(): void {
   }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Server } from '../shared/server';
+
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+
+  color: string;
+  buttonText: string;
+
+  @Input() serverInput: Server;
+
+  ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    }
+    else {
+      this.serverInput.isOnline = false;
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    // console.log(this.serverInput.name, ':', onlineStatus);
+    this.setServerStatus(!onlineStatus);
+  }
+
+}
